e9e9013d330557a308798ae0aa8496da2cc0ee9d
Sudha Rani Redroutu <Sredroutu2607@conestogac.on.ca>
2024-02-03 10:12:55 -0500

    Add project files.

---
+++ Assignmnet-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a type of pet:");
+            Console.WriteLine("1. Cat");
+            Console.WriteLine("2. Dog");
+            Console.WriteLine("3. Rabbit");
+
+            string petType = Console.ReadLine();
+
+            switch (petType)
+            {
+                case "1":
+                    petType = "Cat";
+                    break;
+                case "2":
+                    petType = "Dog";
+                    break;
+                case "3":
+                    petType = "Rabbit";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please choose 1, 2, or 3.");
+                    return;
+            }
+                    Console.WriteLine($"You’ve chosen a {petType}. What would you like to name your pet?");
+                    string petName = Console.ReadLine();
+
+                    Console.WriteLine($"Welcome, {petName}! Let’s take good care of him.");
+        }
+    }
+}
